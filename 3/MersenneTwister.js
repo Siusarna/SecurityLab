7c9c9beb3b856bbf888ad895643b88db5d5846aa
Siusarna <charodeyrap@gmail.com>
2021-11-11 00:20:43 +0200

    add MT19937 v1

--- 3/MersenneTwister.js
+++ 3/MersenneTwister.js
@@ -1,16 +1,60 @@
 'use strict';
 
-const { getRandomNumber } = require('./utils');
+class MersenneTwister {
+    MT = [];
+    index = 0;
+    init = false;
+    seed;
 
-const getGenerator = (m, n, c) => {
-    // FAKE. ITS JUST LCG
-    let seed = BigInt(getRandomNumber(0, 999999999));
-    return () => {
-        seed = (seed * m + c) % n;
-        return BigInt(seed);
+    constructor(seed) {
+        this.seed = seed;
+    }
+
+    generateNumbers() {
+        for (let i = 0; 624 > i; ++i) {
+            let y = (this.MT[i] & 0x80000000) | (this.MT[(i+1) % 624] & 0x7fffffff);
+            this.MT[i] = this.MT[(i + 397) % 624] ^ (y >> 1);
+            if (y % 2 === 1) {
+                this.MT[i] ^= 0x9908b0df;
+            }
+        }
+    }
+
+    initializeGenerator() {
+        this.MT[0] = this.seed;
+        for (let i = 1; 624 > i; ++i) { // loop over each other element
+            this.MT[i] = (0x6c078965 * (this.MT[i - 1] ^ (this.MT[i] >> 30)) + i) & 0xffffffff;
+        }
+    }
+
+    extractNumber() {
+        if (this.index === 0) {
+            if (!this.init) {
+                this.initializeGenerator();
+            }
+            this.generateNumbers();
+        }
+
+        let y = this.MT[this.index];
+        y ^= (y >> 11);
+        y ^= (y <<  7) & 0x9d2c5680;
+        y ^= (y << 15) & 0xefc60000;
+        y ^= (y >> 18);
+        this.index = (this.index + 1) % 624;
+        return y / 0x80000000;
+    }
+
+    randomMT (ubound, flr) {
+        let rnd = this.extractNumber();
+
+        if (ubound !== undefined) {
+            rnd *= ubound;
+        }
+
+        return flr ? ~~rnd : rnd;
     }
 }
 
 module.exports = {
-    getRandom: getGenerator(BigInt(672257317), BigInt(1382843889), BigInt(922337203) )
+    MersenneTwister
 }
