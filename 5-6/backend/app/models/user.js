b87910d767947f8c2532c22f7c5a49c2cc1fa3eb
Siusarna <charodeyrap@gmail.com>
2021-11-20 11:51:01 +0200

    add user model

---
+++ 5-6/backend/app/models/user.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { Joi } = require('celebrate');
+const { DatabaseValidationError } = require('../exceptions/index');
+const humps = require('humps');
+
+const validateSchema = (schema, data) => {
+    if (Array.isArray(data)) {
+        data.forEach(obj => validateSchema(schema, obj));
+    }
+    const v = Joi.validate(data, schema);
+    if (v.error) {
+        const errors = v.error.details.map(err => err.message);
+        throw new DatabaseValidationError(errors.join(', '));
+    }
+}
+
+const table = 'users';
+
+const createSchema = {
+    email: Joi.string().lowercase().required(),
+    password: Joi.string().required(),
+    salt: Joi.string().required()
+}
+
+const create = async (trx, data) => {
+    validateSchema(createSchema, data);
+    const results = await trx
+        .insert(humps.decamelizeKeys(data))
+        .into(table)
+        .returning('*');
+    return humps.camelizeKeys(results[0]);
+}
+
+module.exports = {
+    create
+}
