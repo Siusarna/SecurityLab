a34b85286eef76cb0058ede97188bed97c160e9c
Siusarna <charodeyrap@gmail.com>
2021-11-20 14:14:04 +0200

    add rates limits

--- 5-6/backend/app/routes/auth.js
+++ 5-6/backend/app/routes/auth.js
@@ -2,11 +2,12 @@
 
 const { celebrate } = require('celebrate');
 const validator = require('../validators/auth');
+const rates = require('../rate-limits/auth');
 
 const authController = require('../controllers/auth');
 const wrap = require('../middleware/wrap');
 
 module.exports = app => {
-    app.post('/api/v1/sing-in', celebrate(validator.signIn, validator.options), wrap(authController.signIn));
-    app.post('/api/v1/sing-up', celebrate(validator.signUp, validator.options), wrap(authController.signUp));
+    app.post('/api/v1/sing-in', rates.signInLimiter, celebrate(validator.signIn, validator.options), wrap(authController.signIn));
+    app.post('/api/v1/sing-up', rates.signUpLimiter, celebrate(validator.signUp, validator.options), wrap(authController.signUp));
 }
