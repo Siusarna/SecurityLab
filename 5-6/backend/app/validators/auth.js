8ef358f243ed98ead4030c2001a12ab870a9356c
Siusarna <charodeyrap@gmail.com>
2021-11-21 16:29:33 +0200

    change password error message

--- 5-6/backend/app/validators/auth.js
+++ 5-6/backend/app/validators/auth.js
@@ -4,7 +4,11 @@ const { Joi } = require('celebrate');
 
 const loginInfo = Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,64}$/).required() // Minimum eight and maximum 64 characters, at least one uppercase letter, one lowercase letter, one number and one special character:
+    password: Joi
+        .string()
+        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,64}$/)
+        .required()
+        .label('Minimum eight and maximum 64 characters, at least one uppercase letter, one lowercase letter, one number and one special character')
 })
 
 exports.signIn = {
