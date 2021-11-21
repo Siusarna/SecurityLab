e6643d2a7c78800670d284b6aeeaf5de20357905
Siusarna <charodeyrap@gmail.com>
2021-11-21 13:38:19 +0200

    add validation for email and password

--- 5-6/backend/app/validators/auth.js
+++ 5-6/backend/app/validators/auth.js
@@ -3,8 +3,8 @@
 const { Joi } = require('celebrate');
 
 const loginInfo = Joi.object({
-    email: Joi.string().required(),
-    password: Joi.string().required()
+    email: Joi.string().email().required(),
+    password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,64}$/).required() // Minimum eight and maximum 64 characters, at least one uppercase letter, one lowercase letter, one number and one special character:
 })
 
 exports.signIn = {
