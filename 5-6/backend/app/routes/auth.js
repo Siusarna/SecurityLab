de8b80161b622f2641e5c900698c76e48eb12b37
Siusarna <charodeyrap@gmail.com>
2021-11-21 16:11:00 +0200

    fix routes

--- 5-6/backend/app/routes/auth.js
+++ 5-6/backend/app/routes/auth.js
@@ -8,6 +8,6 @@ const authController = require('../controllers/auth');
 const wrap = require('../middleware/wrap');
 
 module.exports = app => {
-    app.post('/api/v1/sing-in', rates.signInLimiter, celebrate(validator.signIn, validator.options), wrap(authController.signIn));
-    app.post('/api/v1/sing-up', rates.signUpLimiter, celebrate(validator.signUp, validator.options), wrap(authController.signUp));
+    app.post('/api/v1/sign-in', rates.signInLimiter, celebrate(validator.signIn, validator.options), wrap(authController.signIn));
+    app.post('/api/v1/sign-up', rates.signUpLimiter, celebrate(validator.signUp, validator.options), wrap(authController.signUp));
 }
